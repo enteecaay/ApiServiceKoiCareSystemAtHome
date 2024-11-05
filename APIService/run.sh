12bed18b34b43631ad6bb63b7458b01ff8d01639
Nguyễn Tuấn Kiệt <kietnguyen060204@gmail.com>
2024-11-05 11:15:43 +0700

    Update run.sh

--- APIService/run.sh
+++ APIService/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run migrations before starting the app
-dotnet ef database update --no-build
+dotnet ef database update --project ./Domain --startup-project ./APIService
 
 # Start the application
 exec dotnet APIService.dll
